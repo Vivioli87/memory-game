52ca60a05cf4c13d045c8e5f356196151a41a92a
Vivioli87 <vivnightingale@hotmail.com>
2021-03-28 15:26:36 +0000

    added js file, no code just notes planning on what game will need

---
+++ assets/js/game-play.js
@@ -0,0 +1,20 @@
+// function to deal the cards 
+        //within needs to determine difficulty level.
+        //template literals to apply a base format for card layout 3x4 (3 rows x 4 columns on xl, 4 r x 3 x on smaller devices)
+        //add extra row (4 cards) per difficulty
+
+let easyLevel = 12;
+let mediumLevel = 16;
+let hardLevel = 20;
+
+
+
+//function to generate pairs of pictures, randomize in the layot.
+
+//function to flip cards once clicked
+//function to check if 2 cards match - if do hide (add to score count), if dont flip back over.
+
+//function to declare end of game/stop timer when all cards are "hidden" / matched
+
+
+// pop up if user tries to leave game before finished window.beforeunload
