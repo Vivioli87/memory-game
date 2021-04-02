8b50bcfd2ec89d435e28620083d6c139b4d3f915
Vivioli87 <vivnightingale@hotmail.com>
2021-04-02 15:40:10 +0000

    add event listeners to game page buttons. checked and corrected responsiveness

--- assets/js/game-play.js
+++ assets/js/game-play.js
@@ -3,11 +3,27 @@
         //template literals to apply a base format for card layout 3x4 (3 rows x 4 columns on xl, 4 r x 3 x on smaller devices)
         //add extra row (4 cards) per difficulty
 
-let easyLevel = 12;
+let easyLevel = 12; //put these in function so they are not global
 let mediumLevel = 16;
 let hardLevel = 20;
 
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
 
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") == "start") {
+               alert("You clicked start")
+            } else {
+                let difficultyLevel = this.getAttribute("data-type");
+                alert(`You clicked ${difficultyLevel}`);
+            };
+            buttons.classList.remove("active"); //console doesn't like this - **think of better way**
+            this.classList.add("active");
+
+        })
+    }
+})
 
 //function to generate pairs of pictures, randomize in the layot.
 
@@ -18,3 +34,23 @@ let hardLevel = 20;
 
 
 // pop up if user tries to leave game before finished window.beforeunload
+
+function startGame () {
+
+}
+
+function cardsClicked () {
+
+}
+
+function checkPairMatch () {
+
+}
+
+function numberOfClicks () {
+
+}
+
+function timeTaken () {
+
+}
